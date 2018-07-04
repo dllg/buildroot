4ce8fe2e758ac2dfd69f6c13908f509487a9bd11
Daniel Lundborg <daniel.lundborg@se.atlascopco.com>
2018-07-04 16:08:27 +0200

    Starting the build of buildroot with im7sabre target.

--- docker_entry.sh
+++ docker_entry.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-cd /home
-/bin/bash
+BUILD_FOLDER=".buildroot/build"
+DEFCONFIG="freescale_imx7dsabresd_defconfig"
+
+if ! -d $BUILD_FOLDER; then
+    mkdir -p $BUILD_FOLDER
+fi
+
+cd /home/$BUILD_FOLDER
+
+make O=$(pwd) -C /buildroot $DEFCONFIG
+make
